ab35c8f047bef9f6a9d1911a2f2e715356c8829f
KDarch928 <Kayle.Drucker@gmail.com>
2020-02-24 21:16:38 -0600

    created a json file and got javascript to update it when price has changed.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
 const puppeteer = require('puppeteer');
+const data = require("./data.json");
+const fs = require('fs');
 const url = 'https://www.taylormorrison.com/az/phoenix/mesa/the-estates-at-eastmark-summit-collection/floor-plans/crestone'
 
 puppeteer
@@ -15,7 +17,33 @@ puppeteer
   })
   .then(function(html) {
     $('.floorplan-details__stats-priced-from-figure', html).each(function() {
-      console.log($(this).text());
+        var housePrice = $(this).text().replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, '');
+        var covertToNum = parseInt(housePrice);
+        var webPrice = parseInt(housePrice);
+        var curPrice = parseInt(data.curPrice);
+        var origPrice = parseInt(data.origPrice);
+
+        console.log($(this).text());
+        //console.log(price.webPrice);
+
+        console.log("The current price is " + curPrice);
+
+        if(webPrice > curPrice) {
+            //price["webPrice"] = covertToNum;
+            //console.log(price.webPrice);
+            console.log("The new house price is " + data.curPrice);
+            data.curPrice = webPrice.toString();
+            
+            fs.writeFile('./data.json', JSON.stringify(data), (err) => {
+                    if (err) console.log('Error writing file:', err)
+            })
+            console.log("The new house price is " + data.curPrice);
+            var priceChange = webPrice - curPrice;
+            console.log("$"+priceChange);
+        } else if( webPrice === curPrice) {
+            console.log("the price is the same");
+        }
+       
     });
   })
   .catch(function(err) {
